9fdbc4acfd96812a7f7557d1ceca283265795952
nik_nik <nikbox88@gmail.com>
2023-03-17 12:01:19 +0300

    + nodemon & server.listen

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// Run command: 'npx nodemon index.js'
 const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/check", (req, res) => {
+    res.send("Game App running...");
+});
+
+server.listen(3000, () => {
+    console.log("server listening on :3000");
+});
