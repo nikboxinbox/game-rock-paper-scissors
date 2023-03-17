1169ea08d18505ca2e4a1ef47747da8938f57927
nik_nik <nikbox88@gmail.com>
2023-03-17 12:34:10 +0300

    + folder client & its files

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const server = http.createServer(app);
+app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/check", (req, res) => {
-    res.send("Game App running...");
+    res.send("<h1>Game App running...</h1>");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/client/index.html");
 });
 
 server.listen(3000, () => {
