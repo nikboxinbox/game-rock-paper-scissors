09a00973b736ac22a9a5ecbdc99c4e9c0ab5f3fc
nik_nik <nikbox88@gmail.com>
2023-03-31 17:44:36 +0300

    + socket.on connection on server & del old client.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 
 const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+    socket.emit("message", "Hi ASS!");
+});
 
 server.on("error", (err) => {
     console.error("Server error:", err);
