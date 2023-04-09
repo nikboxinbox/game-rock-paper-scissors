615898eb908eaa777fcda648745ba962cd096f8d
nik_nik <nikbox88@gmail.com>
2023-04-09 19:18:31 +0300

    methods of rpsGame

--- server/rpsGame.js
+++ server/rpsGame.js
@@ -1,11 +1,46 @@
 class RpsGame {
-    constructor(p1, p2) {
-        // p1 & p2 - sockets by players
-        this._p1 = p1;
-        this._p2 = p2;
-        [p1, p2].forEach((s) =>
-            s.emit("message", "Камень Ножницы Бумага Начали !")
-        );
+  constructor(p1, p2) {
+    // p1 & p2 - sockets by players
+    this._players = [p1, p2];
+    this._choice = [null, null];
+
+    this._sendMessageToPlayers("Камень Ножницы Бумага. Начали !");
+
+    this._players.forEach((player, idx) => {
+      player.on("choice", (choice) => {
+        this._onChoice(idx, choice);
+      });
+    });
+    // [(p1, p2)].forEach((s) =>
+    //   s.emit("message", "Камень Ножницы Бумага Начали !")
+    // );
+  }
+
+  _sendMessageToPlayer(playerIndex, msg) {
+    this._players[playerIndex].emit("message", msg);
+  }
+
+  _sendMessageToPlayers(msg) {
+    this._players.forEach((player) => {
+      player.emit("message", msg);
+    });
+  }
+
+  _onChoice(playerIndex, choice) {
+    this._choices[playerIndex] = choice;
+    this._sendToPlayer(playerIndex, `Вы выбрали ${turn}`);
+
+    this._checkGameOver();
+  }
+
+  _checkGameOver() {
+    const choices = this._choices;
+
+    if (choices[0] && choices[1]) {
+      this._sendMessageToPlayers("Игра окончена" + choices.join(" : "));
+      this._choices = [null, null];
+      this._sendMessageToPlayers("Следующий раунд!");
     }
+  }
 }
 module.exports = RpsGame;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,37 @@
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const { text } = require("express");
+// const { text } = require("express");
+const RpsGame = require("./rpsGame");
+
 const app = express();
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
 const io = socketio(server);
 
-// let waitingPlayer = null
+let waitingPlayer = null;
 
 io.on("connection", (socket) => {
-    // if (waitingPlayer) {
+  if (waitingPlayer) {
+    new RpsGame(waitingPlayer, socket);
+    waitingPlayer = null;
+  } else {
+    waitingPlayer = socket;
+    waitingPlayer.emit("message", "Ожидаем противника.");
+  }
+  console.log("Socket new connected");
+  socket.emit("message", "Привет, Ты подсоединён!");
 
-    // }
-    console.log("Socket new connected");
-    socket.emit("message", "Привет, Ты подсоединён!");
-
-    socket.on("message", (text) => {
-        io.emit("message", text);
-    });
+  socket.on("message", (text) => {
+    io.emit("message", text);
+  });
 });
 
 server.on("error", (err) => {
-    console.error("Server error:", err);
+  console.error("Server error:", err);
 });
 
 server.listen(3000, () => {
-    console.log("RPS GAME STARTED:  http://localhost:3000/");
+  console.log("RPS GAME STARTED:  http://localhost:3000/");
 });
